393d9b2d86a206ea30ea35c9c0ca6d7061c4e1e3
Zakaria Chowdhury <87743018+ZC82531@users.noreply.github.com>
2024-08-23 11:16:00 -0400

    Added frontend files

---
+++ frontend/src/data/questionnaireQuestions.js
@@ -0,0 +1,86 @@
+export const questions = [
+  {
+    question: "CUNY College Attending:",
+    options: [
+      "Baruch College",
+      "Brooklyn College",
+      "City College of New York",
+      "Hunter College",
+      "Queens College",
+      "College of Staten Island",
+      "Other",
+    ],
+  },
+  {
+    question: "Year of Entry:",
+    options: ["Earlier", "Fall 2024", "Spring 2025", "Other"],
+  },
+  {
+    question:
+      "How aware are you of the various resources CUNY offers to meet students’ essential needs?",
+    options: [
+      "Not aware at all",
+      "Slightly aware",
+      "Somewhat aware",
+      "Very aware",
+    ],
+  },
+  {
+    question:
+      "Have you used or plan to use any of the following CUNY resources?",
+    options: [
+      "Financial Aid Office",
+      "Academic Advising",
+      "Counseling and Wellness Services",
+      "Tutoring and Academic Support",
+      "Career Services",
+      "Food Pantry/Meal Assistance",
+      "Housing Assistance",
+      "Technology Support (e.g., laptop loans, Wi-Fi hotspots)",
+      "Student Organizations/Clubs",
+      "Other",
+    ],
+  },
+  {
+    question:
+      "Which of the following 'essential needs' are most important to you during your time at CUNY?",
+    options: [
+      "Financial Support",
+      "Mental Health and Wellness",
+      "Academic Success/Tutoring",
+      "Career Development",
+      "Housing Stability",
+      "Food Security",
+      "Social Engagement/Clubs",
+      "Technology Access",
+      "Other",
+    ],
+  },
+  {
+    question:
+      "What challenges do you anticipate facing in meeting your essential needs while at CUNY?",
+    options: [
+      "Financial difficulties",
+      "Academic challenges",
+      "Mental health concerns",
+      "Housing insecurity",
+      "Access to food",
+      "Lack of social connections",
+      "Difficulty accessing technology",
+      "Other",
+    ],
+  },
+  {
+    question: "How do you prefer to receive information about CUNY resources?",
+    options: [
+      "Email",
+      "Text message",
+      "Social media (e.g., Instagram, Twitter)",
+      "Mobile app notifications",
+      "College website/portal",
+      "Flyers/posters on campus",
+      "Word of mouth/Peer recommendations",
+      "Other",
+    ],
+  },
+];
